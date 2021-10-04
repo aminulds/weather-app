de8f648e5264da42f115f4f8d84d671c23e6455e
Aminul Islam <aminulds61@gmail.com>
2021-10-04 16:22:05 +0600

    update weather unit convert

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ window.addEventListener('load', () => {
     //selector
     const weatherIcon = document.querySelector('.weatherIcon');
     const locationTimezone = document.querySelector('.location-timezone');
-    const temperature = document.getElementById('temperature');
+    const tempValue = document.getElementById('tempValue');
     const temSummery = document.querySelector('.temperature-description');
+    const unit = document.getElementById('unit');
+    const degreeSection = document.querySelector('.degree-section');
     //Check geo location browser permission
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
@@ -24,15 +26,30 @@ window.addEventListener('load', () => {
                 const {temp} = data.main;
                 const {description, icon} = data.weather[0];
                 const {country} = data.sys;
-                // temperature convert into Celsius
+                // temperature convert
                 const celsius = (temp - 273.15).toFixed(2);
+                const fahrenheit = (celsius * (9 / 5) + 32).toFixed(2);
+                // change Weather unit
+                degreeSection.addEventListener('click', () => {
+                    // check weather unit
+                    if (unit.textContent === 'C'){
+                        unit.textContent = 'F';
+                        tempValue.textContent = fahrenheit;
+                    }
+                    else{
+                        unit.textContent = 'C';
+                        tempValue.textContent = celsius;
+                    }
+                })
+                
                 //show data on html page
                 locationTimezone.textContent = country;
                 weatherIcon.innerHTML = `<img src="icons/${icon}.png" alt="Weather Icon">`;
-                temperature.textContent = celsius;
+                tempValue.textContent = celsius;
                 temSummery.textContent = description;
                 
             })
         })
     }
+
 });
